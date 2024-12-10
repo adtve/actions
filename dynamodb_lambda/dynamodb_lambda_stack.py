5f9d72539c584767359a82de3af231539470e838
adtve <goonzie@gmail.com>
2024-12-10 20:04:52 +0200

    initial

---
+++ dynamodb_lambda/dynamodb_lambda_stack.py
@@ -0,0 +1,50 @@
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_dynamodb,
+    CfnOutput,
+    Stack,
+    App,
+)
+from constructs import Construct
+
+class DynamodbLambdaStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # create dynamo table
+        demo_table = aws_dynamodb.Table(
+            self, "VisitorTimeTable",
+            partition_key=aws_dynamodb.Attribute(
+                name="key",
+                type=aws_dynamodb.AttributeType.STRING
+            )
+        )
+
+        my_lambda = _lambda.Function(self, "my_lambda_function",
+                                     runtime=_lambda.Runtime.PYTHON_3_8,
+                                     handler="main.handler",
+                                     code=_lambda.Code.from_asset("./lambda"))
+
+        f = open("/Users/tdrach/Projects/test/dynamodb-lambda/version.txt", "r")
+        version = f.read() or "0.0"
+
+        my_lambda.add_environment("TABLE_NAME", demo_table.table_name)
+        my_lambda.add_environment("VERSION", version )
+
+        my_lambda_url = _lambda.FunctionUrl(
+            self, 'MyFunctionUrl',
+            function=my_lambda,
+            auth_type=_lambda.FunctionUrlAuthType.NONE
+        )
+
+        demo_table.grant_read_write_data(my_lambda)
+
+        CfnOutput(
+            self, 'FunctionUrlOutput',
+            value=my_lambda_url.url
+        )
+
+app = App()
+Stack(app, "MyStack")
+app.synth()
---
+++ lambda/main.py
@@ -0,0 +1,28 @@
+import os
+import boto3
+
+
+def handler(event, context):
+
+    # Get a reference to the DDB table.
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+
+    # Read the "VISIT COUNT" key (or create it if it doesn't exist)
+    response = table.get_item(Key={"key": "visit_count"})
+    if "Item" in response:
+        visit_count = response["Item"]["value"]
+    else:
+        visit_count = 0
+
+    # Increment the visit count and write it back to the table.
+    new_visit_count = visit_count + 1
+    table.put_item(Item={"key": "visit_count", "value": new_visit_count})
+
+    version = os.environ.get("VERSION", "0.0")
+    response_body = {
+        "message": "Hello World ddd 👋",
+        "version": version.strip(),
+        "visit_count": new_visit_count,
+    }
+    return {"statusCode": 200, "body": response_body}
