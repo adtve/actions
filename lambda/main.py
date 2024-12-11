468d07cbbc9afbea4b769dac1f917fb89b7b2f2a
adtve <goonzie@gmail.com>
2024-12-11 14:21:32 +0200

    lambda modifications

--- lambda/main.py
+++ lambda/main.py
@@ -21,7 +21,7 @@ def handler(event, context):
 
     version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello World 👋",
+        "message": "Hello Walt 👋",
         "version": version.strip(),
         "visit_count": new_visit_count,
     }
